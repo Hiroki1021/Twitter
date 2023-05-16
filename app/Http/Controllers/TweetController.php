0770eb82f3bb97af4f185885e14adc049b4deffb
Hiroki1021 <132648507+Hiroki1021@users.noreply.github.com>
2023-05-16 13:47:15 +0800

    Initial commit

---
+++ app/Http/Controllers/TweetController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tweet;
+
+class TweetController extends Controller
+{
+    //
+    public function showTimelinePage()
+    {
+        return view('timeline');
+    }
+
+    public function postTweet(Request $request)
+    {
+        $validator = $request->validate([
+            'tweet' => ['required', 'string', 'max:280'],
+        ]);
+
+        Tweet::create([
+            'user_id' => 1,
+            'tweet' => $request->tweet,
+        ]);
+
+        return back();
+    }
+}
